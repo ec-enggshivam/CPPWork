7fcdd61f12838879b194ca6961bee31b775885ad
ec-enggshivam <ec_enggshivam@hotmail.com>
2019-09-29 15:29:33 +0530

    list added

---
+++ list/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include <bits\stdc++.h>
+#include <list>
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+typedef list<int> list_int;
+typedef list<string> list_str;
+
+int main(int argc, char** argv) {
+	
+
+	return 0;
+}

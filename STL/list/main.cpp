e6355543abbe0edf2b451fd8c71672d5898cb592
ec-enggshivam <ec_enggshivam@hotmail.com>
2019-09-30 21:00:18 +0530

    Code for list

--- STL/list/main.cpp
+++ STL/list/main.cpp
@@ -9,7 +9,33 @@ typedef list<int> list_int;
 typedef list<string> list_str;
 
 int main(int argc, char** argv) {
+	list_str strings;
+	list_str::iterator it;
 	
-
+	strings.push_back("one");
+	strings.push_back("two");
+	strings.push_back("three");	
+	strings.push_back("four");
+	strings.push_back("five");
+	
+	strings.push_front("0");
+	
+	for(it= strings.begin();it != strings.end();it++){
+		cout<<*it<<" ";
+	}
+	
+	cout<<endl<<endl;
+	
+	it = strings.begin();
+	cout<<*it<<endl;
+	it = strings.erase(it);
+	
+	it = strings.insert(it, "two.five");
+	cout<<*it<<endl;
+	
+	for(it= strings.begin();it != strings.end();it++){
+		cout<<*it<<" ";
+	}
+	
 	return 0;
 }
